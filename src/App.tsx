9beb5093e9f639190b312d4d562bbbe968baaa59
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-11-17 04:25:37 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <h1>Hi!</h1>
+    </Routes>
+  )
+}
+
+export default App
