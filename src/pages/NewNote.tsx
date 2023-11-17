bcd5e3dc1cd1befc5172d38bde48eb055312166b
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-11-17 06:02:40 +0100

    add form layout

---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from "../components/NoteForm";
+
+export const NewNote = () => {
+  return (
+    <section>
+      <h1 className="mb-4">New note</h1>
+      <NoteForm />
+    </section>
+  );
+}
