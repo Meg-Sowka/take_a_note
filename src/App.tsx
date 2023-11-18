c3a1e130d7891cc7ca1ea39b2c5bd5ef959997a4
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-11-18 02:06:44 +0100

    add note details page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { useMemo } from 'react';
 import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './pages/NoteList';
 import { NoteLayout } from './components/NoteLayout';
+import { Note } from './components/Note';
 
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', []);
@@ -42,7 +43,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />} >
-          <Route index element={<h1>Show!</h1>} />
+          <Route index element={<Note />} />
           <Route path="edit" element={<h1>Edit!</h1>} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
